99da824c6752b5361c0f9e69961b079318261602
Oscar Cuadros Linares <ocuadrosl@gmail.com>
2021-06-16 13:17:59 -0300

    read args

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ import os
 import cv2
 import pydicom
 import matplotlib.pyplot as plt
+from pydicom.errors import InvalidDicomError
 from pydicom.pixel_data_handlers.util import apply_voi_lut
-from sklearn.preprocessing import normalize
-
-#import open
+import sys
 
 
 
-def changeDepth(dicom, uType, fix_monochrome=True, gamma=1.0):
+def changeDepth(dicom, depth):
+
+    cvType = cv2.CV_8U if depth == 8 else cv2.CV_16U
+    npType = np.uint8 if depth == 8 else np.uint16
 
     data = apply_voi_lut(dicom.pixel_array, dicom)
 
     # depending on this value, X-ray may look inverted - fix that:
-    if fix_monochrome and dicom.PhotometricInterpretation == "MONOCHROME1":
+    if dicom.PhotometricInterpretation == "MONOCHROME1":
         data = np.amax(data) - data
 
-    depth = np.dtype(uType).itemsize * 8
+    # depth = np.dtype(npType).itemsize * 8
     nBits = (2**depth)-1
 
-    normalizedImg = np.zeros(np.shape(data), dtype=np.dtype(uType))
+    normalizedImg = np.zeros(np.shape(data), dtype=np.dtype(npType))
 
     cvType = cv2.CV_8U if depth == 8 else cv2.CV_16U
     normalizedImg = cv2.normalize(data, dst=normalizedImg, alpha=0, beta=nBits, norm_type=cv2.NORM_MINMAX, dtype=cvType)
@@ -34,24 +36,35 @@ def changeDepth(dicom, uType, fix_monochrome=True, gamma=1.0):
 
 if __name__ == '__main__':
 
+    # setup args from command line
+    datasetPath =  sys.argv[1]
+    datasetWrite =  sys.argv[2]
+    datasetTarget = sys.argv[3]
+    depth = int(sys.argv[4])
+
+    # for histogram equalization
     clahe = cv2.createCLAHE(tileGridSize=(8, 8))
-    for dirname, _, filenames in os.walk('/home/oscar/data/siim-covid19-detection/original/train'):
+
+    for dirname, _, filenames in os.walk(os.path.join(datasetPath, datasetTarget)):
         for filename in filenames:
             #print(os.path.join(dirname, filename))
 
             try:
                 dicom = pydicom.dcmread(os.path.join(dirname, filename))
-                image = changeDepth(dicom, np.uint8)
+
+                # change image depth
+                image = changeDepth(dicom, depth)
 
                 # histogram equalization
                 eqImage = clahe.apply(image)
+
                 # write equalized images
                 fileNameTIFF = os.path.splitext(filename)[0]+".tiff"
-                cv2.imwrite("/home/oscar/data/siim-covid19-detection/uint8/train/"+fileNameTIFF, eqImage)
+                if not cv2.imwrite(os.path.join(datasetWrite, datasetTarget, fileNameTIFF), eqImage):
+                    raise ('Could not write image')
+
                 # plt.imshow(eqImage, cmap=plt.cm.gray)
                 # plt.show()
-            except:
+            except (InvalidDicomError, TypeError, RuntimeError) as raises:
                 print(filename)
-                print("Not a DCM file")
-
-
+                print(raises)
